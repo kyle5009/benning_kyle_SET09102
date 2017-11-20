88f3a04158b3ecb5261a70f611e91b3c92ab73cf
Kyle <benning97@gmail.com>
2017-11-20 18:55:45 +0000

    sender validation
    tweets and emails are now recognised as a sender

--- sourcecode/Euston_Leisure_Messaging/Euston_Leisure_Messaging/Message_input.xaml.cs
+++ sourcecode/Euston_Leisure_Messaging/Euston_Leisure_Messaging/Message_input.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,14 +20,49 @@ namespace Euston_Leisure_Messaging
     /// </summary>
     public partial class Message_input : Window
     {
+
         public Message_input()
         {
             InitializeComponent();
         }
 
+        Sender s = new Sender();
+
+        bool emailCheck(string emailtest)
+        {
+            try
+            {
+                MailAddress testEmail = new MailAddress(emailtest);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
         private void btnSend_Click(object sender, RoutedEventArgs e)
         {
+            s.Tweet = false;
+            s.Sms = false;
+            s.Email = false;
 
+            if (txtSender.Text[0] == '@')
+            {
+                s.Tweet = true;
+                MessageBox.Show("This is a tweet");
+            }
+
+            if (txtSender.Text.Contains('@'))
+            {
+                s.Sms = true;
+            }
+
+            if (emailCheck(txtSender.Text))
+            {
+                s.Email = true;
+                MessageBox.Show("This is an email");
+            }
         }
     }
 }
---
+++ sourcecode/Euston_Leisure_Messaging/Euston_Leisure_Messaging/Sender.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euston_Leisure_Messaging
+{
+    public class Sender
+    {
+        private bool email;
+        private bool tweet;
+        private bool sms;
+
+        public bool Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public bool Tweet
+        {
+            get
+            {
+                return tweet;
+            }
+            set
+            {
+                tweet = value;
+            }
+        }
+
+        public bool Sms
+        {
+            get
+            {
+                return sms;
+            }
+            set
+            {
+                sms = value;
+            }
+        }
+    }
+}
